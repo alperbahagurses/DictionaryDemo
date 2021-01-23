e6ac58611654a6019bcf8942505e9680e6a25623
alperbahagurses <bahagurses23@gmail.com>
2021-01-23 16:13:37 +0300

    Add project files.

---
+++ MyDictionary.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DictionaryDemo
+{
+    class MyDictionary<T1,T2>
+    {
+        T1[] ids;
+        T2[] names;
+        public MyDictionary()
+        {
+            ids = new T1[0];
+            names = new T2[0];
+        }
+        public void Add(T1 id,T2 name)
+        {
+            T1[] tempIds = ids;
+            T2[] tempNames = names;
+            ids = new T1[ids.Length+1];
+            names = new T2[names.Length+1];
+            for (int i = 0; i < tempIds.Length; i++)
+            {
+                ids[i] = tempIds[i];
+                names[i] = tempNames[i];
+            }
+            ids[ids.Length - 1] = id;
+            names[names.Length - 1] = name;
+        }
+
+        public T1[] Ids
+        {
+            get { return ids; }
+        }
+
+        public T2[] Names
+        {
+            get { return names; }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DictionaryDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> category = new MyDictionary<int, string>();
+            category.Add(1, "Elektronik");
+            category.Add(2,"Kitap");
+            category.Add(3, "Bahçe");
+            category.Add(4, "Kırtasiye");
+            category.Add(5, "Ayakkabı");
+            category.Add(6, "Aksesuar");
+
+            foreach (var id in category.Ids)
+            {
+                Console.WriteLine("Kategori : "+id);
+            }
+            Console.WriteLine("-----------------------------------");
+            foreach (var name in category.Names)
+            {
+                Console.WriteLine("Kategori Adı : "+name);
+            }
+        }
+    }
+}
